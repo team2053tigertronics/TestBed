765513cb07e46da4977dcda3aef23a15d85f123f
tigertronics-coder <team2053tigertronics@gmail.com>
2020-01-28 21:18:56 -0500

    added other intake/shooter commands

--- src/main/cpp/Robot.cpp
+++ src/main/cpp/Robot.cpp
@@ -9,15 +9,13 @@
 
 #include <iostream>
 
-#include <frc/smartdashboard/SmartDashboard.h>
-#include <ctre/phoenix/motorcontrol/can/TalonFX.h>
-#include <ctre/phoenix/motorcontrol/can/TalonSRX.h>
-#include <rev/CANSparkMax.h>
-#include <frc/XboxController.h>
 
 void Robot::RobotInit() {
     shooterMotorLeft.ConfigFactoryDefault();
     shooterMotorRight.ConfigFactoryDefault();
+    intakeMotor.ConfigFactoryDefault();
+    feederMotor.ConfigFactoryDefault();
+    conveyorMotor.ConfigFactoryDefault();
 
     shooterMotorRight.ConfigSelectedFeedbackSensor(ctre::phoenix::motorcontrol::FeedbackDevice::IntegratedSensor);
     shooterMotorRight.Config_kF(0, kFF);
@@ -26,7 +24,7 @@ void Robot::RobotInit() {
     shooterMotorRight.Config_kD(0, kP);
     shooterMotorRight.Config_IntegralZone(0, kIz);
     shooterMotorRight.ConfigPeakOutputForward(kMaxOutput);
-    shooterMotorRight.ConfigPeakOutputReverse(kMinOutput);
+    shooterMotorRight.ConfigPeakOutputReverse(0);
 
     shooterMotorLeft.Follow(shooterMotorRight);
     shooterMotorLeft.SetInverted(true);
@@ -40,6 +38,9 @@ void Robot::RobotInit() {
     frc::SmartDashboard::PutNumber("Min Output", kMinOutput);
 
     SetPoint = 0.0;
+    otherSet = 0.0;
+    conveyorSet = 0.0;
+    feederSet = 0.0;
 }
 
 void Robot::RobotPeriodic() {
@@ -79,18 +80,29 @@ void Robot::TeleopPeriodic() {
     }
 
     if(m_stick.GetYButtonPressed()) {
-        SetPoint += 500;
+        SetPoint = 200;
     }
     if(m_stick.GetAButtonPressed()) {
-        SetPoint -= 500;
+        otherSet = 0;
+        conveyorSet = 0;
     }
     if(m_stick.GetBButtonPressed()) {
-        SetPoint += 200;
+        otherSet = .5;
+        conveyorSet = .5;
     }
     if(m_stick.GetXButtonPressed()) {
-        SetPoint -= 200;
+        SetPoint = 0;
+    }
+    if(m_stick.GetStartButtonPressed()) {
+        feederSet = .5;
+    }
+    if(m_stick.GetStartButtonReleased()) {
+        feederSet = 0;
     }
     if(m_stick.GetBackButtonPressed()) {
+        conveyorSet = 0;
+        feederSet = 0;
+        otherSet = 0;
         SetPoint = 0;
     }
 
@@ -98,17 +110,20 @@ void Robot::TeleopPeriodic() {
     bool rightPressed = m_stick.GetBumper(frc::XboxController::JoystickHand::kRightHand);
     if(leftPressed) 
     {
-      m_feederMotor.Set(ctre::phoenix::motorcontrol::ControlMode::PercentOutput, 1);
+      //m_feederMotor.Set(ctre::phoenix::motorcontrol::ControlMode::PercentOutput, 1);
     }
     if(rightPressed)
     {
-      m_feederMotor.Set(ctre::phoenix::motorcontrol::ControlMode::PercentOutput, -1);
+      //m_feederMotor.Set(ctre::phoenix::motorcontrol::ControlMode::PercentOutput, -1);
     }
     if(!leftPressed && !rightPressed) {
-      m_feederMotor.Set(ctre::phoenix::motorcontrol::ControlMode::PercentOutput, 0);
+      //m_feederMotor.Set(ctre::phoenix::motorcontrol::ControlMode::PercentOutput, 0);
     }
 
-    shooterMotorRight.Set(ctre::phoenix::motorcontrol::ControlMode::Velocity, ConvertRPMToTicksPer100Ms(SetPoint));
+    shooterMotorRight.Set(ctre::phoenix::motorcontrol::ControlMode::Velocity, (int)ConvertRPMToTicksPer100Ms(SetPoint));
+    intakeMotor.Set(ctre::phoenix::motorcontrol::ControlMode::PercentOutput, otherSet);
+    feederMotor.Set(ctre::phoenix::motorcontrol::ControlMode::PercentOutput, feederSet);
+    conveyorMotor.Set(ctre::phoenix::motorcontrol::ControlMode::PercentOutput, conveyorSet);
 
     frc::SmartDashboard::PutNumber("Commanded Shooter Velocity", SetPoint);
     frc::SmartDashboard::PutNumber("Left Motor Velocity", ConvertTicksPer100MsToRPM(shooterMotorLeft.GetSelectedSensorVelocity()));
@@ -120,11 +135,11 @@ void Robot::TestPeriodic() {
 }
 
 int Robot::ConvertRPMToTicksPer100Ms(double rpm) {
-    return (rpm  / 600) * (sensorResolution / gearRatio);
+    return (rpm  / 600.0) * sensorResolution;
 }
 
 double Robot::ConvertTicksPer100MsToRPM(int ticksPer100ms) {
-    return (ticksPer100ms * 600) / (sensorResolution / gearRatio);
+    return (ticksPer100ms * 600) / sensorResolution;
 }
 
 #ifndef RUNNING_FRC_TESTS
--- src/main/include/Robot.h
+++ src/main/include/Robot.h
@@ -11,6 +11,11 @@
 
 #include <frc/TimedRobot.h>
 
+#include <frc/smartdashboard/SmartDashboard.h>
+#include <ctre/phoenix/motorcontrol/can/TalonFX.h>
+#include <ctre/phoenix/motorcontrol/can/TalonSRX.h>
+#include <frc/XboxController.h>
+
 class Robot : public frc::TimedRobot {
  public:
   void RobotInit() override;
@@ -22,20 +27,28 @@ class Robot : public frc::TimedRobot {
   void TestPeriodic() override;
 
  private:
-  static const int shooterMotorLeftID = 6;
-  static const int shooterMotorRightID = 7;
+  static const int shooterMotorLeftID = 4;
+  static const int shooterMotorRightID = 26;
   static const int feederMotorID = 3;
+  static const int conveyorMotorID = 1;
+  static const int intakeMotorID = 2;
+
   ctre::phoenix::motorcontrol::can::TalonFX shooterMotorLeft{shooterMotorLeftID};
   ctre::phoenix::motorcontrol::can::TalonFX shooterMotorRight{shooterMotorRightID};
-  ctre::phoenix::motorcontrol::can::TalonSRX m_feederMotor{feederMotorID};
+  ctre::phoenix::motorcontrol::can::TalonFX conveyorMotor{conveyorMotorID};
+  ctre::phoenix::motorcontrol::can::TalonFX intakeMotor{intakeMotorID};
+  ctre::phoenix::motorcontrol::can::TalonSRX feederMotor{feederMotorID};
 
   frc::XboxController m_stick{0};
 
   double kP = 6e-5, kI = 1e-6, kD = 0, kIz = 0, kFF = 0.000015, kMaxOutput = 1.0, kMinOutput = -1.0;
   const double MaxRPM = 6380;
   double SetPoint = 0;
-  double sensorResolution = 4096;
-  double gearRatio = 1/2;
+  double otherSet = 0;
+  double feederSet = 0;
+  double conveyorSet = 0;
+  double sensorResolution = 2048;
+  double gearRatio = 1.0/2.0;
 
   int ConvertRPMToTicksPer100Ms(double rpm);
   double ConvertTicksPer100MsToRPM(int ticksPer100ms);
