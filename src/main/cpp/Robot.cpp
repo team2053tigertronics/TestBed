95bc0983476cf45b1443e45d3eab8a503d29ba00
tigertronics-coder <team2053tigertronics@gmail.com>
2020-01-30 18:12:47 -0500

    increased setpoint

--- src/main/cpp/Robot.cpp
+++ src/main/cpp/Robot.cpp
@@ -80,7 +80,7 @@ void Robot::TeleopPeriodic() {
     }
 
     if(m_stick.GetYButtonPressed()) {
-        SetPoint = 2000;
+        SetPoint = 6000;
     }
     if(m_stick.GetAButtonPressed()) {
         otherSet = 0;
